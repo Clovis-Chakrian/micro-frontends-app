079989c3b6159b27695382cca3db55501fab1394
Clóvis Chakrian <clovischakriandev@gmail.com>
2024-08-22 09:22:39 -0300

    chore: iniciando projeto com single spa para estudos para a oficina

---
+++ app-vue/vue.config.js
@@ -0,0 +1,13 @@
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+    transpileDependencies: true,
+    configureWebpack: {
+        output: {
+            libraryTarget: "system",
+        },
+    },
+});
